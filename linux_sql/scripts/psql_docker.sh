f27c8ed2606faea937782a5c90736bf9ba788d50
Fraser Raney <fraserr01@gmail.com>
2025-10-16 20:45:06 +0000

    updated comments

--- linux_sql/scripts/psql_docker.sh
+++ linux_sql/scripts/psql_docker.sh
@@ -34,7 +34,7 @@ case $cmd in
 	docker run --name jrvs-psql -e POSTGRES_PASSWORD=$db_password  \
 	-e POSTGRES_USER=$db_username -d -v pgdata:/var/lib/postgresql/data \
 	-p 5432:5432 postgres:9.6-alpine
-  # Make sure you understand what's `$?`
+  # `$?` is the exit code of the previous command
 	exit $?
 	;;
 
