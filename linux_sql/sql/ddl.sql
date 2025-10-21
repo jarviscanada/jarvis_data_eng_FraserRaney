ab50cb6d9e84f5fe555a953e4eb1e65d9491d27d
Fraser Raney <fraserr01@gmail.com>
2025-10-21 14:45:02 +0000

    initial commit for ddl.sql

---
+++ linux_sql/sql/ddl.sql
@@ -0,0 +1,34 @@
+-- switch to host_agent database from command line
+-- psql -h [hostname] -U [username] -d [database] -f [file]
+-- Example:
+-- psql -h localhost -U postgres -d host_agent -f sql/ddl.sql
+
+-- Create a host_info table if it doesn't exist
+CREATE TABLE IF NOT EXISTS PUBLIC.host_info
+(
+    id               SERIAL NOT NULL,
+    hostname         VARCHAR NOT NULL,
+    cpu_number       INT2 NOT NULL,
+    cpu_architecture VARCHAR NOT NULL,
+    cpu_model        VARCHAR NOT NULL,
+    cpu_mhz          FLOAT8 NOT NULL,
+    l2_cache         INT4 NOT NULL,
+    "timestamp"      TIMESTAMP NULL,
+    total_mem        INT4 NULL,
+    CONSTRAINT host_info_pk PRIMARY KEY (id),
+    CONSTRAINT host_info_un UNIQUE (hostname)
+);
+
+-- Create a host_usage table
+CREATE TABLE IF NOT EXISTS PUBLIC.host_usage
+(
+    "timestamp"    TIMESTAMP NOT NULL,
+    host_id        SERIAL NOT NULL,
+    memory_free    INT4 NOT NULL,
+    cpu_idle       INT2 NOT NULL,
+    cpu_kernel     INT2 NOT NULL,
+    disk_io        INT4 NOT NULL,
+    disk_available INT4 NOT NULL,
+    CONSTRAINT host_usage_host_info_fk FOREIGN KEY (host_id) REFERENCES
+        host_info(id)
+);
