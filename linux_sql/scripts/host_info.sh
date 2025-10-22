d8e52c2088ce510abaaed0e9d6327647f864f45e
Fraser Raney <fraserr01@gmail.com>
2025-10-22 19:09:58 +0000

    added skeleton for host_info.sh

---
+++ linux_sql/scripts/host_info.sh
@@ -0,0 +1,38 @@
+# Setup and validate arguments (again, don't copy comments)
+psql_host=$1
+psql_port=$2
+db_name=$3
+psql_user=$4
+psql_password=$5
+
+# Check # of args
+if [ "$#" -ne 5 ]; then
+    echo "Illegal number of parameters"
+    exit 1
+fi
+
+# save hostname as a variable
+hostname=$(hostname -f)
+
+# save the number of CPUs to a variable
+lscpu_out=`lscpu`
+
+# hardware info
+hostname=$(hostname -f)
+cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
+cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
+cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" |  awk -F': *' '{print $2}' | xargs)
+cpu_mhz=$(echo "$cpu_model" | egrep -o '[0-9]+(\.[0-9]+)?' | awk '{printf "%.3f", $1 * 1000}' | xargs)
+l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | egrep -o '[0-9]+' | awk 'NR==2' | xargs)
+total_mem=$(cat /proc/meminfo | egrep 'MemTotal' | egrep -o '[0-9]+' |xargs)
+timestamp=$(date +"%Y-%m-%d %H:%M:%S") # current timestamp in `2019-11-26 14:40:19` format; use `date` cmd
+
+# PSQL command: Inserts hardware info into host_info table
+# Note: be careful with double and single quotes
+insert_stmt="INSERT INTO host_info(timestamp, ...) VALUES('$timestamp', #todo....
+
+#set up env var for pql cmd
+export PGPASSWORD=$psql_password
+#Insert date into a database
+psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
+exit $?
