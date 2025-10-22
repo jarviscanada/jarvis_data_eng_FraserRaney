186d338256d0bd4cedc17b2936ae42ca58535268
Fraser Raney <fraserr01@gmail.com>
2025-10-22 20:18:59 +0000

    updated comments in host_info.sh

--- linux_sql/scripts/host_info.sh
+++ linux_sql/scripts/host_info.sh
@@ -1,11 +1,11 @@
-# Setup and validate arguments (again, don't copy comments)
+# setup arguments
 psql_host=$1
 psql_port=$2
 db_name=$3
 psql_user=$4
 psql_password=$5
 
-# Check # of args
+# validate arguments
 if [ "$#" -ne 5 ]; then
     echo "Illegal number of parameters"
     exit 1
@@ -17,7 +17,7 @@ hostname=$(hostname -f)
 # save the number of CPUs to a variable
 lscpu_out=`lscpu`
 
-# hardware info
+# parse hardware info
 hostname="$hostname"
 cpu_number=$(echo "$lscpu_out"  | egrep "^CPU\(s\):" | awk '{print $2}' | xargs)
 cpu_architecture=$(echo "$lscpu_out"  | egrep "^Architecture:" | awk '{print $2}' | xargs)
@@ -25,15 +25,16 @@ cpu_model=$(echo "$lscpu_out"  | egrep "^Model name:" |  awk -F': *' '{print $2}
 cpu_mhz=$(echo "$cpu_model" | egrep -o '[0-9]+(\.[0-9]+)?' | awk '{printf "%.3f", $1 * 1000}' | xargs)
 l2_cache=$(echo "$lscpu_out"  | egrep "^L2 cache:" | egrep -o '[0-9]+' | awk 'NR==2' | xargs)
 total_mem=$(cat /proc/meminfo | egrep 'MemTotal' | egrep -o '[0-9]+' |xargs)
-timestamp=$(date +"%Y-%m-%d %H:%M:%S") # current timestamp in `2019-11-26 14:40:19` format; use `date` cmd
+timestamp=$(date +"%Y-%m-%d %H:%M:%S") # current timestamp in `2019-11-26 14:40:19` format
 
-# PSQL command: Inserts hardware info into host_info table
+# PSQL command: Inserts hardware into host_info table
 insert_stmt="INSERT INTO host_info(id, hostname, cpu_number, cpu_architecture, cpu_model, cpu_mhz, l2_cache, total_mem, \"timestamp\") \
 VALUES(1, '$hostname', '$cpu_number', '$cpu_architecture', '$cpu_model', '$cpu_mhz', '$l2_cache', '$total_mem', '$timestamp') \
 ON CONFLICT (id) DO UPDATE SET hostname = EXCLUDED.hostname, cpu_number = EXCLUDED.cpu_number, cpu_architecture = EXCLUDED.cpu_architecture, cpu_model = EXCLUDED.cpu_model, cpu_mhz = EXCLUDED.cpu_mhz, l2_cache = EXCLUDED.l2_cache, total_mem = EXCLUDED.total_mem, \"timestamp\" = EXCLUDED.\"timestamp\";"
 
-#set up env var for pql cmd
+# set up env var for pql cmd
 export PGPASSWORD=$psql_password
-#Insert date into a database
+
+# insert hardware info into database
 psql -h $psql_host -p $psql_port -d $db_name -U $psql_user -c "$insert_stmt"
 exit $?
