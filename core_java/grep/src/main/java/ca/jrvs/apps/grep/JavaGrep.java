818733055a03fe83fd66a2b534aff5488707626c
Fraser Raney <fraserr01@gmail.com>
2025-11-18 17:01:05 +0000

    added JavaGrepImpl and JavaGrepImplTest

--- core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/grep/JavaGrep.java
@@ -8,12 +8,14 @@ public interface JavaGrep {
 
   /**
    * Top level search workflow
+   *
    * @throws IOException
    */
   void process() throws IOException;
 
   /**
    * Traverse a given directory and return all files
+   *
    * @param rootDir input directory
    * @return files under the rootDir
    */
@@ -22,15 +24,15 @@ public interface JavaGrep {
   /**
    * Read a file and return all the lines
    * <p>
-   * This method uses a {@link java.io.BufferedReader} wrapped
-   * around a {@link java.io.FileReader}. The {@code FileReader} is a
-   * character-stream reader that reads directly from the file using the
-   * platform's default character encoding. {@link java.io.BufferedReader}
-   * adds buffering (8 KB by default).
+   * This method uses a {@link java.io.BufferedReader} wrapped around a {@link java.io.FileReader}.
+   * The {@code FileReader} is a character-stream reader that reads directly from the file using the
+   * platform's default character encoding. {@link java.io.BufferedReader} adds buffering (8 KB by
+   * default).
    * </p>
    * <p>
    * Note: This implementation does not expose encoding choice ? it assumes default encoding.
    * </p>
+   *
    * @param inputFile file to be read
    * @return lines
    * @throws IllegalArgumentException if a given inputFile is not a file
@@ -39,6 +41,7 @@ public interface JavaGrep {
 
   /**
    * check if a line contains the regex patter (passed by the user)
+   *
    * @param line input string
    * @return true if there is a match
    */
@@ -47,13 +50,13 @@ public interface JavaGrep {
   /**
    * Write lines to a file
    * <p>
-   * This method uses {@link java.io.FileOutputStream} to write raw bytes to the
-   * file, wrapped by an {@link java.io.OutputStreamWriter} to convert
-   * characters into bytes using a character encoding. By default, the
-   * {@code OutputStreamWriter} uses the platform?s default charset. A
-   * {@link java.io.BufferedWriter} reduces the number of I/O writes by holding
-   * characters in memory and flushing them in larger chunks
+   * This method uses {@link java.io.FileOutputStream} to write raw bytes to the file, wrapped by an
+   * {@link java.io.OutputStreamWriter} to convert characters into bytes using a character encoding.
+   * By default, the {@code OutputStreamWriter} uses the platform?s default charset. A
+   * {@link java.io.BufferedWriter} reduces the number of I/O writes by holding characters in memory
+   * and flushing them in larger chunks
    * </p>
+   *
    * @param lines matched line
    * @throws IOException if write failed
    */
@@ -65,9 +68,9 @@ public interface JavaGrep {
 
   String getRegex();
 
-  String setRegex(String regex);
+  void setRegex(String regex);
 
   String getOutFile();
 
-  String setOutFile(String outFile);
+  void setOutFile(String outFile);
 }
