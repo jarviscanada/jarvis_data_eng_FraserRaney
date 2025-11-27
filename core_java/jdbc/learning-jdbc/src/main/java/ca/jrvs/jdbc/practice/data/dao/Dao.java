99cbde70eb52c2b0e89fe9efd58311973c11fa24
Fraser Raney <fraserr01@gmail.com>
2025-11-27 01:43:13 +0000

    added DAO

---
+++ core_java/jdbc/learning-jdbc/src/main/java/ca/jrvs/jdbc/practice/data/dao/Dao.java
@@ -0,0 +1,13 @@
+package ca.jrvs.jdbc.practice.data.dao;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface Dao <T, Id extends UUID>{
+  List<T> getAll();
+  T create(T entity);
+  Optional<T> getOne(Id id);
+  T update(T entity);
+  void delete(Id id);
+}
