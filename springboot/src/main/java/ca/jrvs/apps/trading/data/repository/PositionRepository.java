a0a985f6b521ee4f7ed5cffb3e0de193f5a4a84b
Fraser Raney <fraserr01@gmail.com>
2025-12-23 22:56:41 +0000

    trader service skeleton and some implementation

--- springboot/src/main/java/ca/jrvs/apps/trading/data/repository/PositionRepository.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/data/repository/PositionRepository.java
@@ -2,8 +2,10 @@ package ca.jrvs.apps.trading.data.repository;
 
 import ca.jrvs.apps.trading.data.entity.Position;
 import ca.jrvs.apps.trading.data.entity.PositionKey;
+import java.util.List;
 
 
 public interface PositionRepository extends ReadOnlyRepository<Position, PositionKey> {
-
+  List<Position> findAllByIdAccountId(Integer accountId);
+  Integer countAllByIdAccountId(Integer accountId);
 }
