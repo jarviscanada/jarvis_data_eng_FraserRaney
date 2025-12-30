2d20de087107a0e486372e2d63c84be5c327a758
Fraser Raney <fraserr01@gmail.com>
2025-12-30 19:34:39 +0000

    dashboard controller

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
@@ -1,9 +1,17 @@
 package ca.jrvs.apps.trading.controller;
 
+import ca.jrvs.apps.trading.data.entity.PortfolioView;
+import ca.jrvs.apps.trading.data.entity.TraderAccountView;
 import ca.jrvs.apps.trading.service.DashboardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 @Controller
 @RequestMapping("/dashboard")
@@ -16,5 +24,27 @@ public class DashboardController {
     this.dashboardService = dashboardService;
   }
 
+  @ResponseBody
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(value = "/profile/traderId/{traderId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public TraderAccountView getAccount(@PathVariable Integer traderId) {
+    try {
+      return dashboardService.getTraderAccount(traderId);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
+
+
+  @ResponseBody
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(value = "/portfolio/traderId/{traderId}", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public PortfolioView getPortfolio(@PathVariable Integer traderId) {
+    try {
+      return dashboardService.getPortfolioViewByTraderId(traderId);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
 
 }
