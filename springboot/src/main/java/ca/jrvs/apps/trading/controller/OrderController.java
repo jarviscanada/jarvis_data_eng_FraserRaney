30692cd0e573a88cdc3555a58db4393bb89581e0
Fraser Raney <fraserr01@gmail.com>
2025-12-30 00:38:11 +0000

    ordercontroller

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/OrderController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/OrderController.java
@@ -1,5 +1,34 @@
 package ca.jrvs.apps.trading.controller;
 
+import ca.jrvs.apps.trading.data.entity.MarketOrder;
+import ca.jrvs.apps.trading.data.entity.SecurityOrder;
+import ca.jrvs.apps.trading.service.OrderService;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+@RequestMapping("/order")
 public class OrderController {
 
+  private OrderService orderService;
+
+  public OrderController(OrderService orderService) {
+    this.orderService = orderService;
+  }
+
+  @PostMapping("/marketOrder")
+  @ResponseStatus(HttpStatus.CREATED)
+  @ResponseBody
+  public SecurityOrder postMarketOrder(@RequestBody MarketOrder marketOrder) {
+    try {
+      return orderService.executeMarketOrder(marketOrder);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
 }
