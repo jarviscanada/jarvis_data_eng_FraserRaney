a65b19215c6a81fb5077cdcd844a724a97451cbd
Fraser Raney <fraserr01@gmail.com>
2026-02-03 17:01:14 +0000

    added route to controller

--- springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
+++ springboot/src/main/java/ca/jrvs/apps/trading/controller/DashboardController.java
@@ -3,6 +3,8 @@ package ca.jrvs.apps.trading.controller;
 import ca.jrvs.apps.trading.data.entity.PortfolioView;
 import ca.jrvs.apps.trading.data.entity.TraderAccountView;
 import ca.jrvs.apps.trading.service.DashboardService;
+import ca.jrvs.apps.trading.service.TraderAccountService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -17,11 +19,14 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 @RequestMapping("/dashboard")
 public class DashboardController {
 
+  private final TraderAccountService traderAccountService;
   private DashboardService dashboardService;
 
   @Autowired
-  public DashboardController(DashboardService dashboardService) {
+  public DashboardController(DashboardService dashboardService,
+      TraderAccountService traderAccountService) {
     this.dashboardService = dashboardService;
+    this.traderAccountService = traderAccountService;
   }
 
   @ResponseBody
@@ -42,6 +47,17 @@ public class DashboardController {
   public PortfolioView getPortfolio(@PathVariable Integer traderId) {
     try {
       return dashboardService.getPortfolioViewByTraderId(traderId);
+    } catch (Exception e) {
+      throw ResponseExceptionUtil.getResponseStatusException(e);
+    }
+  }
+
+  @ResponseBody
+  @ResponseStatus(HttpStatus.OK)
+  @GetMapping(value = "/traders", produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
+  public List<TraderAccountView> getTraders() {
+    try {
+      return traderAccountService.getAllTraders();
     } catch (Exception e) {
       throw ResponseExceptionUtil.getResponseStatusException(e);
     }
